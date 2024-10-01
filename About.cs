a04e3613eb7090607da3541a738a8ec1db0a5095
pippaamy <pippa_austin@live.com>
2024-10-01 09:52:25 +0100

    info for repo

---
+++ About.cs
@@ -0,0 +1,12 @@
+﻿namespace ASPNET
+{
+    public class About
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Mission { get; set; }
+        public string OriginStory { get; set; }
+
+    }
+    }
---
+++ Rules.cs
@@ -0,0 +1,11 @@
+﻿namespace ASPNET
+{
+    public class Rules
+    {
+
+        public int Id { get; set; }
+        public string Rule { get; set; }
+        public string Description { get; set; }
+
+    }
+}
