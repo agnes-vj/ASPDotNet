bfd0a82d82a63d9532f48b2ee585f0607544ae0e
joel.wright.cs@gmail.com <joel.wright.cs@gmail.com>
2024-12-17 10:37:02 +0000

    Implement get for about page

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+using ASPNET;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/about", () =>
+{
+    string about = File.ReadAllText("Resources/About.json");
+    return JsonSerializer.Deserialize<About>(about);
+});
 
 app.Run();
